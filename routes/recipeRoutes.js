9d02d9123b1a9c07b4e9080371ed44ad6095f12b
Yujeong Choung <yjchoung10@gmail.com>
2021-02-06 16:02:17 -0800

    Revert "initial commit & authorization added"
    This reverts commit 61b804c6242279cc78e35f4aead7a669ad7c3915.

--- routes/recipeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const recipeController = require('../controllers/recipeController.js');
-
-const router = express.Router();
-
-router.get('/', recipeController.getRecipes);
-
-module.exports = router;
