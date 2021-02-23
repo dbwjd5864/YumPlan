144129337871f698fc8e95446e9e305a3bc40d67
Yujeong Choung <yjchoung10@gmail.com>
2021-02-22 21:51:48 -0800

    deploy API

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(express.json({ limit: '10mb', extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.send('Hello to YumPlan API');
+});
+
 // Connect to DB and server
 connection.once('open', () => {
   console.log('connected to database');
@@ -32,9 +36,9 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/meal', mealRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   );
 }
