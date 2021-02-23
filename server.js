a73425dd16545f904f2e53971f50e0cbf4258da2
Yujeong Choung <yjchoung10@gmail.com>
2021-02-23 00:22:07 -0800

    cors

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const mealRouter = require('./routes/mealRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // Middleware
+app.use(cors());
 app.use(helmet());
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json({ limit: '10mb', extended: true }));
-app.use(cors());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
