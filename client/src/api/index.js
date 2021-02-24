b7dacb2491f2cccef7327db5bb03c8da28b96c3b
Yujeong Choung <yjchoung10@gmail.com>
2021-02-23 17:33:19 -0800

    .

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // User API
 const userUrl = 'https://yumplan.herokuapp.com/api/v1/user';
-const proxy = '/api/v1/user';
+// const proxy = '/api/v1/user';
 
 export const isLoggedIn = () => axios.get(`${userUrl}`);
 
@@ -15,7 +15,7 @@ export const logout = () => axios.get(`${userUrl}/logout`);
 ///////////////////////////
 // Meal API
 const mealUrl = 'https://yumplan.herokuapp.com/api/v1/meal';
-const mealProxy = '/api/v1/meal';
+// const mealProxy = '/api/v1/meal';
 
 export const getAllMeals = () => axios.get(`${mealUrl}`);
 
