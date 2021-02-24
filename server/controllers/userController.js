dbaacd894ebf950d2e91245c5702e4a5130ab842
Yujeong Choung <yjchoung10@gmail.com>
2021-02-24 13:04:19 -0800

    typo

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,7 +13,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    SameSite = 'none',
+    SameSite: 'none',
     secure: req.headers['x-forwarded-proto'] === 'https' || req.secure || true,
   });
 
