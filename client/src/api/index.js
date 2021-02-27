c99ea4980d282793f2d6c3e5e4727e5ff124aa01
Yujeong Choung <yjchoung10@gmail.com>
2021-02-26 16:34:15 -0800

    changed package

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,14 +3,11 @@ import axios from 'axios';
 // User API
 const userUrl = '/api/v1/user';
 
-export const isLoggedIn = () =>
-  axios.get(`${userUrl}`, { withCredentials: true });
+export const isLoggedIn = () => axios.get(`${userUrl}`);
 
-export const signup = (newUser) =>
-  axios.post(`${userUrl}/signup`, newUser, { withCredentials: true });
+export const signup = (newUser) => axios.post(`${userUrl}/signup`, newUser);
 
-export const login = (user) =>
-  axios.post(`${userUrl}/login`, user, { withCredentials: true });
+export const login = (user) => axios.post(`${userUrl}/login`, user);
 
 export const logout = () => axios.get(`${userUrl}/logout`);
 
@@ -23,8 +20,7 @@ export const getAllMeals = () => axios.get(`${mealUrl}`);
 export const updateLikeCount = (mealId, updateLike) =>
   axios.patch(`${mealUrl}/${mealId}?like=${updateLike}`);
 
-export const getMealPlanner = () =>
-  axios.get(`${mealUrl}/planner`, { withCredentials: true });
+export const getMealPlanner = () => axios.get(`${mealUrl}/planner`);
 
 export const getWeeklyPlanner = (week) =>
   axios.get(`${mealUrl}/planner/weekly-plan/${week}`, {
@@ -32,7 +28,7 @@ export const getWeeklyPlanner = (week) =>
   });
 
 export const createMealPlan = (mealPlan) =>
-  axios.post(`${mealUrl}/planner`, mealPlan, { withCredentials: true });
+  axios.post(`${mealUrl}/planner`, mealPlan);
 
 export const updateMealPlan = (mealId, updatedMealPlan) =>
   axios.patch(`${mealUrl}/planner/${mealId}`, updatedMealPlan, {
@@ -40,13 +36,12 @@ export const updateMealPlan = (mealId, updatedMealPlan) =>
   });
 
 export const deleteMealPlan = (mealId) =>
-  axios.delete(`${mealUrl}/planner/${mealId}`, { withCredentials: true });
+  axios.delete(`${mealUrl}/planner/${mealId}`);
 
 // Meal Favorite
 
-export const getAllFavorites = () =>
-  axios.get(`${mealUrl}/favorites`, { withCredentials: true });
+export const getAllFavorites = () => axios.get(`${mealUrl}/favorites`);
 export const addFavorite = (mealId) =>
-  axios.patch(`${mealUrl}/${mealId}/favorites`, { withCredentials: true });
+  axios.patch(`${mealUrl}/${mealId}/favorites`);
 export const updateFavorite = (favoriteId) =>
-  axios.patch(`${mealUrl}/favorites/${favoriteId}`, { withCredentials: true });
+  axios.patch(`${mealUrl}/favorites/${favoriteId}`);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,8 +17,7 @@ const createSendToken = (user, statusCode, req, res) => {
         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      sameSite: 'none',
-      secure: true,
+      secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
     })
     .status(statusCode)
     .json({
