2a36c8d1a38c580bf7a136c26b38ccfd0af4db0f
Yujeong Choung <yjchoung10@gmail.com>
2021-02-26 21:53:52 -0800

    static path changed

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/meal', mealRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname, '../client/build'));
+  app.use(express.static('../client/build'));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '/../client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   );
 }
